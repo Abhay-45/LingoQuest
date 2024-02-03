f3c181634b0d8a2816979b08b10a4e123790da7b
Abhay Maurya <abhaymaurya452002@gmail.com>
2024-02-03 17:44:31 +0000

    connection

--- client/src/module/form/SimpleForm.js
+++ client/src/module/form/SimpleForm.js
@@ -1,123 +1,121 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react';
 import ChatBot from 'react-simple-chatbot';
-import Review from './Review';
-import styled from 'styled-components'
 
-class SimpleForm extends Component {
-    render() {
-        const config ={
-            width: "700px",
-            height: "700px", 
-          };
+const SimpleForm = () => {
+  const [translatedText, setTranslatedText] = useState('');
+  const [chatBotSteps, setChatBotSteps] = useState([
+    {
+      id: '1',
+      message: 'Write a sentence in English that you would like to translate!',
+      trigger: 'translate',
+    },
+    {
+      id: 'translate',
+      user: true,
+      validator: (value) => {
+        handleProcess(value);
+        return true;
+      },
+      trigger: 'showTranslation',
+    },
+    {
+      id: 'showTranslation',
+      message: 'Initial message', // This will be updated with the translated text
+      trigger: '4'
+    },
+    {
+        id: '4',
+        message: 'Write a sentence in English that you would like to translate!',
+        end: true
+      },
+  ]);
 
-        return (
-            <ChatBot
-               
-                steps={[
-                    {
-                        id: '1',
-                        message: 'What is your name?',
-                        trigger: 'name',
-                    },
-                    {
-                        id: 'name',
-                        user: true,
-                        trigger: '3',
-                    },
-                    {
-                        id: '3',
-                        message: 'Hi {previousValue}! What is your gender?',
-                        trigger: 'gender',
-                    },
-                    {
-                        id: 'gender',
-                        options: [
-                            { value: 'male', label: 'Male', trigger: '5' },
-                            { value: 'female', label: 'Female', trigger: '5' },
-                        ],
-                    },
-                    {
-                        id: '5',
-                        message: 'How old are you?',
-                        trigger: 'age',
-                    },
-                    {
-                        id: 'age',
-                        user: true,
-                        trigger: '7',
-                        validator: (value) => {
-                            if (isNaN(value)) {
-                                return 'value must be a number';
-                            } else if (value < 0) {
-                                return 'value must be positive';
-                            } else if (value > 120) {
-                                return `${value}? Come on!`;
-                            }
-
-                            return true;
-                        },
-                    },
-                    {
-                        id: '7',
-                        message: 'Great! Check out your summary',
-                        trigger: 'review',
-                    },
-                    {
-                        id: 'review',
-                        component: <Review />,
-                        asMessage: true,
-                        trigger: 'update',
-                    },
-                    {
-                        id: 'update',
-                        message: 'Would you like to update some field?',
-                        trigger: 'update-question',
-                    },
-                    {
-                        id: 'update-question',
-                        options: [
-                            { value: 'yes', label: 'Yes', trigger: 'update-yes' },
-                            { value: 'no', label: 'No', trigger: 'end-message' },
-                        ],
-                    },
-                    {
-                        id: 'update-yes',
-                        message: 'What field would you like to update?',
-                        trigger: 'update-fields',
-                    },
-                    {
-                        id: 'update-fields',
-                        options: [
-                            { value: 'name', label: 'Name', trigger: 'update-name' },
-                            { value: 'gender', label: 'Gender', trigger: 'update-gender' },
-                            { value: 'age', label: 'Age', trigger: 'update-age' },
-                        ],
-                    },
-                    {
-                        id: 'update-name',
-                        update: 'name',
-                        trigger: '7',
-                    },
-                    {
-                        id: 'update-gender',
-                        update: 'gender',
-                        trigger: '7',
-                    },
-                    {
-                        id: 'update-age',
-                        update: 'age',
-                        trigger: '7',
-                    },
-                    {
-                        id: 'end-message',
-                        message: 'Thanks! Your data was submitted successfully!',
-                        end: true,
-                    },
-                ]}
-                {...config}
-            />
-        );
+  useEffect(() => {
+    if (translatedText) {
+      updateChatbotStep('showTranslation', translatedText);
     }
-}
+  }, [translatedText]);
+
+  useEffect(() => {
+    console.log("German_text", translatedText);
+  }, [translatedText]);
+
+  
+
+  
+  const handleProcess = async (englishText) => {
+    try {
+      const response = await fetch('http://127.0.0.1:5000/translate', {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          english: englishText,
+        }),
+      });
+
+      const result = await response.json();
+      console.log(result);
+      setTranslatedText(result.german_text);
+      // Update the chatbot step with the translated text
+      updateChatbotStep('showTranslation', result.german_text);
+
+      
+    } catch (error) {
+      console.error('Error processing translation:', error);
+    }
+  };
+
+  const updateChatbotStep = (stepId, newMessage) => {
+    const updatedSteps = chatBotSteps.map((step) => {
+      if (step.id === stepId) {
+        return { ...step, message: newMessage };
+      }
+      return step;
+    });
+    console.log("Inside the updateChatbotStep", translatedText)
+
+    // Update the chatbot steps
+    console.log(updatedSteps)
+    setChatBotSteps(updatedSteps);
+  };
+
+  const config = {
+    width: "700px",
+    height: "700px",
+  };
+
+//   const chatBotSteps = [
+//     {
+//       id: '1',
+//       message: 'Write a sentence in English that you would like to translate!',
+//       trigger: 'translate',
+//     },
+//     {
+//       id: 'translate',
+//       user: true,
+//       validator: (value) => {
+//         handleProcess(value);
+//         return true;
+//       },
+//       trigger: 'showTranslation',
+//     },
+//     {
+//       id: 'showTranslation',
+//       message: 'Initial message', // This will be updated with the translated text
+//       end: true,
+//     },
+//   ];
+
+  return (
+    <ChatBot
+      steps={chatBotSteps}
+      {...config}
+      
+    />
+  );
+};
 
 export default SimpleForm;
